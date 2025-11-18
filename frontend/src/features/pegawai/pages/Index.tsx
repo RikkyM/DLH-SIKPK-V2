a62f4363fed747f41b90f3ef0a1d6f99f302cc14
RikkyM <rikky.mahendra54@gmail.com>
2025-11-19 04:32:30 +0700

    add: search input pegawai

--- frontend/src/features/pegawai/pages/Index.tsx
+++ frontend/src/features/pegawai/pages/Index.tsx
@@ -107,10 +107,11 @@ const Index = () => {
                 name="per_page"
                 id="per_page"
                 className="h-full w-full text-sm px-3 py-1.5 focus:outline-none border border-gray-300 rounded"
-                value={perPage}
+                value={""}
                 onChange={(e) => handlePerPageChange(Number(e.target.value))}
               >
-                <option value="5">5</option>
+                <option value="" ></option>
+                <option value="5" >5</option>
                 <option value="10">10</option>
                 <option value="25">25</option>
                 <option value="50">50</option>
