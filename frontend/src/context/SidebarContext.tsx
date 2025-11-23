76adc332fe623af8260f8ce67f2c2c51640d7c34
RikkyM <rikky.mahendra54@gmail.com>
2025-11-24 05:50:41 +0700

    changes: browserrouter

--- frontend/src/context/SidebarContext.tsx
+++ frontend/src/context/SidebarContext.tsx
@@ -4,6 +4,7 @@ type SidebarContextType = {
   isOpen: boolean;
   toggleSidebar: () => void;
   closeSidebar: () => void;
+  notMobile: () => void;
 };
 
 export const SidebarContext = createContext<SidebarContextType | null>(null);
--- frontend/src/providers/SidebarProvider.tsx
+++ frontend/src/providers/SidebarProvider.tsx
@@ -1,14 +1,33 @@
 import { SidebarContext } from "@/context/SidebarContext";
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 
 export const SidebarProvider = ({ children }: { children: ReactNode }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth >= 1024; // lg breakpoint = 1024px
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isLargeScreen = window.innerWidth >= 1024;
+      setIsOpen(isLargeScreen);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const toggleSidebar = () => setIsOpen((prev) => !prev);
   const closeSidebar = () => setIsOpen(false);
+  const notMobile = () => setIsOpen(true);
 
   return (
-    <SidebarContext.Provider value={{ isOpen, toggleSidebar, closeSidebar }}>
+    <SidebarContext.Provider value={{ isOpen, toggleSidebar, closeSidebar, notMobile }}>
       {children}
     </SidebarContext.Provider>
   );
