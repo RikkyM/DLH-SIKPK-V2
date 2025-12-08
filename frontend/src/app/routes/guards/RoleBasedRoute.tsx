d71538aaab5264ebf075d50461c8600f94606668
RikkyM <rikky.mahendra54@gmail.com>
2025-12-08 07:30:59 +0700

    fix: modal pegawai edit

--- frontend/src/app/routes/guards/RoleBasedRoute.tsx
+++ frontend/src/app/routes/guards/RoleBasedRoute.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/features/auth";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 type RoleBasedRouteProps = {
   allowedRoles: string[];
@@ -7,8 +7,13 @@ type RoleBasedRouteProps = {
 
 export const RoleBasedRoute = ({ allowedRoles }: RoleBasedRouteProps) => {
   const { user } = useAuth();
+  const location = useLocation();
 
   const hasPermission = user && allowedRoles.includes(user.role);
+
+  if (!user) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
 
   if (!hasPermission) {
     return <Navigate to="/dashboard" replace />;
