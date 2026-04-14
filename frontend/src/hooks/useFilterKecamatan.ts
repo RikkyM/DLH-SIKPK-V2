130dd1908bd48a681e3af6fb61831f80cfba8f2b
RikkyM <rikky.mahendra54@gmail.com>
2026-04-14 11:43:16 +0700

    fix(petugas): input kecamatan dan keluarahan

--- frontend/src/hooks/useFilterKecamatan.ts
+++ frontend/src/hooks/useFilterKecamatan.ts
@@ -1,53 +1,66 @@
 import { http } from "@/services/api/http";
-import { useCallback, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+// import { useCallback, useEffect, useState } from "react";
 
 type KecamatanType = {
   kodeKecamatan: string;
   namaKecamatan: string;
 };
 
-type KecamatanState = {
-  data: KecamatanType[] | null;
-  loading: boolean;
-  error: string | null;
-};
+// type KecamatanState = {
+//   data: KecamatanType[] | null;
+//   loading: boolean;
+//   error: string | null;
+// };
 
 export const useFilterKecamatan = () => {
-  const [state, setState] = useState<KecamatanState>({
-    data: null,
-    loading: false,
-    error: null,
+  // const [state, setState] = useState<KecamatanState>({
+  //   data: null,
+  //   loading: false,
+  //   error: null,
+  // });
+
+  // const getKecamatan = useCallback(async () => {
+  //   setState((prev) => ({
+  //     ...prev,
+  //     loading: true,
+  //   }));
+  //   try {
+  //     const res = await http.get("/api/v1/kecamatan");
+
+  //     setState((prev) => ({
+  //       ...prev,
+  //       data: res.data.data,
+  //       loading: false,
+  //     }));
+  //   } catch {
+  //     setState((prev) => ({
+  //       ...prev,
+  //       loading: false,
+  //       error: "Gagal mendapatkan data kecamatan.",
+  //     }));
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   void getKecamatan();
+  // }, [getKecamatan]);
+
+  const {
+    data,
+    isLoading: loading,
+    error,
+  } = useQuery<KecamatanType[]>({
+    queryKey: ["kecamatan"],
+    queryFn: async () => {
+      const { data } = await http.get("/api/v1/kecamatan");
+      return data.data;
+    },
   });
 
-  const getKecamatan = useCallback(async () => {
-    setState((prev) => ({
-      ...prev,
-      loading: true,
-    }));
-    try {
-      const res = await http.get("/api/v1/kecamatan");
-
-      setState((prev) => ({
-        ...prev,
-        data: res.data.data,
-        loading: false,
-      }));
-    } catch {
-      setState((prev) => ({
-        ...prev,
-        loading: false,
-        error: "Gagal mendapatkan data kecamatan.",
-      }));
-    }
-  }, []);
-
-  useEffect(() => {
-    void getKecamatan();
-  }, [getKecamatan]);
-
   return {
-    datas: state.data,
-    loading: state.loading,
-    error: state.error,
+    datas: data,
+    loading,
+    error,
   };
 };
--- frontend/src/hooks/useFilterKelurahan.ts
+++ frontend/src/hooks/useFilterKelurahan.ts
@@ -1,5 +1,6 @@
 import { http } from "@/services/api/http";
-import { useCallback, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+// import { useCallback, useEffect, useState } from "react";
 
 type KelurahanType = {
   kodeKelurahan: string;
@@ -7,49 +8,61 @@ type KelurahanType = {
   kodeKecamatan: string;
 };
 
-type KelurahanState = {
-  data: KelurahanType[] | null;
-  loading: boolean;
-  error: string | null;
-};
+// type KelurahanState = {
+//   data: KelurahanType[] | null;
+//   loading: boolean;
+//   error: string | null;
+// };
 
 export const useFilterKelurahan = () => {
-  const [state, setState] = useState<KelurahanState>({
-    data: null,
-    loading: false,
-    error: null,
+  // const [state, setState] = useState<KelurahanState>({
+  //   data: null,
+  //   loading: false,
+  //   error: null,
+  // });
+
+  // const getKelurahan = useCallback(async () => {
+  //   setState((prev) => ({
+  //     ...prev,
+  //     loading: true,
+  //   }));
+
+  //   try {
+  //     const res = await http.get("/api/v1/kelurahan");
+
+  //     setState((prev) => ({
+  //       ...prev,
+  //       data: res.data.data,
+  //       loading: false,
+  //     }));
+  //   } catch {
+  //     setState((prev) => ({
+  //       ...prev,
+  //       loading: false,
+  //       error: "Gagal mendapatkan data kelurahan.",
+  //     }));
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   void getKelurahan();
+  // }, [getKelurahan]);
+
+  const {
+    data,
+    isLoading: loading,
+    error,
+  } = useQuery<KelurahanType[]>({
+    queryKey: ["kelurahan"],
+    queryFn: async () => {
+      const { data } = await http.get("/api/v1/kelurahan");
+      return data.data;
+    },
   });
 
-  const getKelurahan = useCallback(async () => {
-    setState((prev) => ({
-      ...prev,
-      loading: true,
-    }));
-
-    try {
-      const res = await http.get("/api/v1/kelurahan");
-
-      setState((prev) => ({
-        ...prev,
-        data: res.data.data,
-        loading: false,
-      }));
-    } catch {
-      setState((prev) => ({
-        ...prev,
-        loading: false,
-        error: "Gagal mendapatkan data kelurahan.",
-      }));
-    }
-  }, []);
-
-  useEffect(() => {
-    void getKelurahan();
-  }, [getKelurahan]);
-
   return {
-    datas: state.data,
-    loading: state.loading,
-    error: state.error,
+    datas: data,
+    loading,
+    error,
   };
 };
